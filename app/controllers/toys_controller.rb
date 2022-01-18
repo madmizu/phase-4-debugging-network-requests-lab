b5dd817b4cef6f7f4a784d0e8da6cea82f242dcd
Madison Shimizu <mhshimizu@outlook.com>
2022-01-17 17:18:53 -0700

    completed

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,7 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity 
 
   def index
     toys = Toy.all
@@ -7,25 +9,44 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create!(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    toy = find_toy
+    toy.update!(toy_params)
+    render json: toy  
+  end
+
+  def increment_likes
+    toy = find_toy
+    toy.update!(likes: toy.likes + 1)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
+
+  def render_unprocessable_entity (invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  end
   
   def toy_params
     params.permit(:name, :image, :likes)
+  end
+
+  def find_toy
+    Toy.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
   end
 
 end
